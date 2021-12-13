9cc38ad500ef77bac76203e93ad5ec04cbed4976
John Adams Jr <john.c.adams32@gmail.com>
2021-12-13 12:02:42 -0700

    TODO list and beginning program data

---
+++ coffee_machine.py
@@ -0,0 +1,20 @@
+from coffee_data import menu, resources
+# TODO: 1. Print logos to start machine
+# TODO: 2. Ask the user what drink they would like
+# TODO: 3. Check the resources to determine if enough to make drink
+# TODO: 4. If not enough resources prompt user to ask again for type of drink
+# TODO: 5. Allow user to end the machine with the secret word "off"
+# TODO: 6. Ask the user to input coins.
+# TODO: 7. Ask how many quarters.
+# TODO: 8. Ask how many dimes.
+# TODO: 9. Ask how many nickels.
+# TODO: 10. Ask how many pennies
+# TODO: 11. If the user has enough or more coins make the drink and issue change.
+# TODO: 12. If not enough coins, refund coins and prompt the user again for type of drink.
+# TODO: 13. Allow user to check resources with secret word "report"
+# TODO: 14. Once the machine takes in money keep a running total of the money
+
+
+
+
+
