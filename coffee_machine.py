e09ad91170edd52ec1bc01705894b0f5978811ba
John Adams Jr <john.c.adams32@gmail.com>
2021-12-13 13:53:51 -0700

    seven items remaining on the todo list

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,14 +1,22 @@
 from coffee_data import menu, resources
+from art import logo
 # TODO: 1. Print logos to start machine
+print(logo)
 # TODO: 2. Ask the user what drink they would like
+customer_drink_selection = input("What drink would you like today? (espresso/latte/cappuccino): ")
 # TODO: 3. Check the resources to determine if enough to make drink
 # TODO: 4. If not enough resources prompt user to ask again for type of drink
 # TODO: 5. Allow user to end the machine with the secret word "off"
 # TODO: 6. Ask the user to input coins.
+print("Please Insert Coins")
 # TODO: 7. Ask how many quarters.
+inserted_quarters = input("How many quarters?: ")
 # TODO: 8. Ask how many dimes.
+inserted_dimes = input("How many dimes?: ")
 # TODO: 9. Ask how many nickels.
+inserted_nickels = input("How many nickels?: ")
 # TODO: 10. Ask how many pennies
+inserted_pennies =input("How many pennies?: ")
 # TODO: 11. If the user has enough or more coins make the drink and issue change.
 # TODO: 12. If not enough coins, refund coins and prompt the user again for type of drink.
 # TODO: 13. Allow user to check resources with secret word "report"
